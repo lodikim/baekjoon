81894df4207f467752c2c3e1c9ab7b2592c9e051
lodikim <lodingkim@gmail.com>
2021-12-23 11:33:20 +0900

    step2 [c]

---
+++ step_by_step/step_2/c/2884.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+    int H, M;
+    scanf("%d %d", &H, &M);
+
+    if(M>=45)
+        printf("%d %d", H, M-45);
+    else if(H>0)
+        printf("%d %d", H-1, 60-(45-M));
+    else
+        printf("23 %d", 60-(45-M));
+}
